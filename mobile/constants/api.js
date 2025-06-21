77bc1da23d69bec87aad20a11a01ca267cbd465c
Prakhar Maheshwari <prakharmaheshwari96@gmail.com>
2025-06-21 20:44:21 +0530

    updated url

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -2,7 +2,7 @@
 // Choose the appropriate option based on your testing environment:
 
 // OPTION 1: For Android Emulator
-export const API_URL = "http://10.0.2.2:5000/api";
+export const API_URL = "https://react-native-recipe-app-yme4.onrender.com/api";
 
 // OPTION 2: For iOS Simulator
 // export const API_URL = "http://localhost:5001/api";
